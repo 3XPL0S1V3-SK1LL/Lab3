ccdd227bec0e4dca15d06a68c2e220a955e0a365
3XPL0S1V3-SK1LL <153466684+3XPL0S1V3-SK1LL@users.noreply.github.com>
2023-12-23 05:16:36 +0300

    Delete Door.java

--- Door.java
+++
@@ -1,5 +0,0 @@
-public interface Door{ //задаем поведение двери
-    void setStatus(boolean status); // у нее можеть быть статус: закрытая или открытая (true or false)
-    boolean getStatus();
-    void printStatus(); // так же мы можем узнать открыта она или закрыта
-}
