cc8af593dbaa75252b0d0ea417da717e0f132e4e
3XPL0S1V3-SK1LL <153466684+3XPL0S1V3-SK1LL@users.noreply.github.com>
2023-12-28 15:40:33 +0300

    Add files via upload

---
+++ IntStairs.java
@@ -0,0 +1,10 @@
+package inanimateobjects;
+
+public interface IntStairs {
+    static void creak(){
+        System.out.println("*Скрип*");
+    }
+    void breakDown();
+    void beRapaired();
+}
+
